9102183dfaf69bd9b67d532716922a632fca3db8
gillianrhett <gillianrhett@gmail.com>
2026-02-16 19:31:02 -0800

    input validation for duration works; taking a break

--- 210-lab-11.cpp
+++ 210-lab-11.cpp
@@ -2,13 +2,15 @@
 #include <iostream>
 #include <string> // for getline to take string with spaces
 #include <iomanip> // for displaying data e.g. price with 2 decimal places
+#include <stdexcept> // for input validation
+#include <limits> // for clearing invalid input
 using namespace std;
 
 struct Trip {
 // this is for planning and storing info about group trips
     string destination; // name of location they will travel to    
     int duration; // number of days
-    double price; // price per person
+    double price = 1.0; // price per person - using default while testing other stuff
     string* participants = nullptr; // dynamic array of people's names
 
     ~Trip () {
@@ -21,17 +23,15 @@ struct Trip {
 };
 
 // functions for working with the Trip struct
-Trip createTrip();
 void inputTripData(Trip*);
 void displayTripData(Trip*);
-int getValidInput(int);
-double getValidInput(double);
 
 int main() {
-    int num;
-    cout << "Enter number: "; // TESTING
-    cin >> num; // TESTING
-    getValidInput(num); // TESTING
+    Trip* april = new Trip;
+    inputTripData(april);
+
+    delete april;
+
     return 0;
 }
 
@@ -45,27 +45,12 @@ void inputTripData(Trip* trip) {
     
     cout << "Enter the duration (number of days): ";
     // input validation: duration is a number > 0
-    
-    cout << "Enter the price per person: ";
-    // input validation: price is a number > 0
-
-    cout << "Enter the first participant's name: ";
-    // make the array
-
-    // loop where they can enter more names; enter 0 to finish
-}
-
-void displayTripData(Trip* trip) {
-
-}
-
-int getValidInput(int numIn) {
     bool validInput = false;
     while (!validInput) {
-        try // I haven't had a lot of practice with this yet so it's taking some time
+        try
         {
-            cin >> numIn;
-            if (cin.fail() || numIn < 1)
+            cin >> trip->duration;
+            if (cin.fail() || trip->duration < 1)
                 throw invalid_argument("enter a number at least 1: ");
             validInput = true;
         }
@@ -73,13 +58,21 @@ int getValidInput(int numIn) {
         {
             //cerr << e.what() << '\n'; // VSCode autocompleted this
             cout << "Error: " << e.what();
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
             // then repeat the while loop so they can enter another number
         }
     }
-    return numIn;
+    // TODO get and validate price
+    cout << "Enter the price per person: ";
+    // input validation: price is a number > 0
+
+    cout << "Enter the first participant's name: ";
+    // TODO make the array
+
+    // TODO loop where they can enter more names; enter 0 to finish
 }
 
-double getValidInput(double numIn) {
+void displayTripData(Trip* trip) {
 
-    return numIn;
 }
